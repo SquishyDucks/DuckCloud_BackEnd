464c930ec0c055608a3b38f548f5cd3063cdaf90
Howard Reith <howard.reith@gmail.com>
2018-07-17 10:08:49 -0400

    Add Heroku CURL scripts
    -Add scripts for performing change password, sign in, sign out,
    and sign up through CURL to our deployed Heroku back end site.

---
+++ scripts/heroku-auth/sign-in.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+API="https://tranquil-thicket-56465.herokuapp.com"
+URL_PATH="/sign-in"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
