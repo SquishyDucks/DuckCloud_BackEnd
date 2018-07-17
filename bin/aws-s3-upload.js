ba68bc15392dd10256a5ba4d8b6c3b23c1a18284
tazpowell <hellotazuko@gmail.com>
2018-07-17 10:52:08 -0400

    Add AWS upload code
    - add bin script
    - add lib file with upload methods/promises

---
+++ bin/aws-s3-upload.js
@@ -0,0 +1,17 @@
+'use script'
+
+const s3Upload = require('../lib/s3Upload')
+const mime = require('mime-types')
+
+const file = {
+  path: process.argv[2],
+  mimetype: mime.lookup(process.argv[2]),
+  originalname: process.argv[2]
+}
+
+s3Upload(file)
+  .then((s3Response) => {
+    console.log('s3Response is ', s3Response)
+    return s3Response
+  })
+  .catch(console.error)
